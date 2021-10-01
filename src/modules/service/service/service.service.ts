fa99a67eeec5c8f1df7a7ee8a69e44a37235f42f
Irlen Turlykhanov <irlen.turlykhanov@gmail.com>
2021-10-01 16:59:09 +0000

    Develop service attach

--- src/modules/service/service/service.service.ts
+++ src/modules/service/service/service.service.ts
@@ -8,8 +8,8 @@ import { ServiceSearch } from '../model/service.schema';
 export class ServiceService {
     constructor(
         @Inject('DATABASE_CONNECTION') private database: Db,
-        @Inject('SMARTSEARCH_CONNECTION') private client
-        ) {}
+        @Inject('SMARTSEARCH_CONNECTION') private client,
+    ) {}
 
     private get serviceCollection() {
         return this.database.collection('service');
@@ -20,8 +20,48 @@ export class ServiceService {
     }
 
     async findOne(service: Partial<Service>): Promise<Service> {
-        const serviceResponce = await this.serviceCollection.findOne<Service>(service);
+        const serviceResponce = await this.serviceCollection.findOne<Service>(
+            service,
+        );
         return serviceResponce;
+    }
+
+    async updateOne(args: {
+        findField: (keyof Service)[];
+        findValue: any[];
+        updateField: (keyof Service)[];
+        updateValue: any[];
+        method: '$set' | '$inc' | '$addToSet' | '$push' | '$pull';
+    }) {
+        const { findField, findValue, updateField, updateValue, method } = args;
+        const findQuery: any = {};
+        findField.map((val, ind) => (findQuery[val] = findValue[ind]));
+        const updateFieldsValues: any = {};
+        updateField.map(
+            (val, ind) => (updateFieldsValues[val] = updateValue[ind]),
+        );
+        const updateQuery = {
+            [method]: updateFieldsValues,
+        };
+        const updateService = await this.serviceCollection.findOneAndUpdate(
+            findQuery,
+            updateQuery,
+            { returnDocument: 'after' },
+        );
+        return updateService.value as Service;
+    }
+
+    async findOneWithOptions(args: {
+        fields: (keyof Service)[];
+        values: any[];
+    }) {
+        const { fields, values } = args;
+        const findQuery: any = {};
+        fields.map((val, ind) => (findQuery[val] = values[ind]));
+        const service = (await this.serviceCollection.findOne(
+            findQuery,
+        )) as any as Service;
+        return service;
     }
 
     async create(args: CreateService) {
@@ -34,7 +74,7 @@ export class ServiceService {
         const insertService = await this.serviceCollection.insertOne(service);
         const searchService: ServiceSearch = {
             ...service,
-            _id: service._id.toHexString()
+            _id: service._id.toHexString(),
         };
         await this.searchCollection.create(searchService);
         service._id = insertService.insertedId;
