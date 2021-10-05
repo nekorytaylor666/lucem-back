0d50b591d7d6ccea241edfa8b03deb5fb71733fd
irlen.turlykhanov <irlen.turlykhanov@gmail.com>
2021-10-06 02:23:53 +0600

    feat: get upcoming bookings

--- src/modules/booking/resolver/booking.resolver.ts
+++ src/modules/booking/resolver/booking.resolver.ts
@@ -1,8 +1,9 @@
 import { UseGuards } from "@nestjs/common";
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Args, Int, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { Roles } from "src/modules/helpers/auth/auth.roles";
 import { CurrentUserGraph, PreAuthGuard } from "src/modules/helpers/auth/auth.service";
 import { User } from "src/modules/user/model/user.interface";
+import { paginate } from "src/utils/paginate";
 import { BookingGraph } from "../model/booking.model";
 import { CreateBooking } from "../model/createBooking.args";
 import { BookingService } from "../service/booking.service";
@@ -21,5 +22,21 @@ export class BookingResolver {
         const createBooking = await this.bookingService.create({...args, userId: user._id.toHexString()});
         const bookingResponce = new BookingGraph({...createBooking});
         return bookingResponce
+    }
+
+    @Query(() => [BookingGraph])
+    @Roles('doctor', 'admin')
+    @UseGuards(PreAuthGuard)
+    async getUpcomingBookings(
+        @Args('page', { type: () => Int }) page: number
+    ) {
+        const currentDate = new Date();
+        const bookingsCursor = await this.bookingService.findWithOptionsCursor({
+            fields: ['startDate'],
+            values: [{ $gt: currentDate }]
+        });
+        const bookings = await paginate({ cursor: bookingsCursor, page, elementsPerPage: 10 });
+        const bookingsResponce = bookings.map((val) => new BookingGraph({...val}));
+        return bookingsResponce;
     }
 }
--- src/modules/helpers/auth/auth.service.ts
+++ src/modules/helpers/auth/auth.service.ts
@@ -7,9 +7,11 @@ import {
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { Collection, ObjectId } from 'mongodb';
+import { AdminService } from 'src/modules/admin/service/admin.service';
 import { DoctorService } from 'src/modules/doctor/service/doctor.service';
 import { UserService } from 'src/modules/user/service/user.service';
 import { matchRoles } from 'src/utils/matchRoles';
+import { TokenRoles } from '../token/token.interface';
 import { TokenService } from '../token/token.service';
 
 @Injectable()
@@ -18,25 +20,36 @@ export class PreAuthGuard implements CanActivate {
         private tokenService: TokenService,
         private userService: UserService,
         private reflector: Reflector,
-        private doctorService: DoctorService
+        private doctorService: DoctorService,
+        private adminService: AdminService,
     ) {}
 
     async canActivate(context: ExecutionContext) {
         const request = GqlExecutionContext.create(context).getContext().req;
         const { authorization } = request.headers;
-        const roles = this.reflector.get<string[]>('roles', context.getHandler());
-        request.requestURL = `${request.protocol}://${request.rawHeaders[1]}`
+        const roles = this.reflector.get<string[]>(
+            'roles',
+            context.getHandler(),
+        );
+        request.requestURL = `${request.protocol}://${request.rawHeaders[1]}`;
         if (roles[0] === 'none') return true;
         if (!authorization) return false;
         try {
             const payload = this.tokenService.verify({ token: authorization });
             const checkRoles = matchRoles(payload.role, roles);
             if (!checkRoles) return false;
-            let dbService: UserService | DoctorService = this.userService;
-            if (payload.role === 'doctor') dbService = this.doctorService
-            const user = await dbService.findOne({
-                _id: new ObjectId(payload._id),
-            });
+            const user =
+                payload.role === TokenRoles.User
+                    ? await this.userService.findOne({
+                          _id: new ObjectId(payload._id),
+                      })
+                    : payload.role === TokenRoles.Doctor
+                    ? await this.doctorService.findOne({
+                          _id: new ObjectId(payload._id),
+                      })
+                    : await this.adminService.findOne({
+                          _id: new ObjectId(payload._id),
+                      });
             if (!user) return false;
             request.user = user;
 
@@ -46,7 +59,6 @@ export class PreAuthGuard implements CanActivate {
         }
     }
 }
-
 
 export const CurrentUserGraph = createParamDecorator(
     async (data: unknown, context: ExecutionContext) => {
