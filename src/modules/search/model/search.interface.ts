5f4949a6d80daf4ea6b2943dc2ddfc0044b9e1bd
irlen.turlykhanov <irlen.turlykhanov@gmail.com>
2021-10-21 22:16:43 +0600

    fix: rename files

--- src/modules/search/model/search.interface.ts
+++ src/modules/search/model/search.interface.ts
@@ -1,3 +1,4 @@
+import { Desease } from 'src/modules/deseases/model/desease.interface';
 import { Doctor } from 'src/modules/doctor/model/doctor.interface';
 import { Service } from 'src/modules/service/model/service.interface';
 
