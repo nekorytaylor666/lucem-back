dcd4a1ec97b816a2ff0bd849544b04cf1934d504
irlen.turlykhanov <irlen.turlykhanov@gmail.com>
2021-10-29 02:05:08 +0600

    fix: add results resilvers to module

--- src/modules/appointmentBlank/appointmentBlank.module.ts
+++ src/modules/appointmentBlank/appointmentBlank.module.ts
@@ -4,11 +4,18 @@ import { MongoModule } from '../helpers/database/mongo.module';
 import { UploadFileModule } from '../helpers/uploadFiles/uploadFiles.module';
 import { SessionModule } from '../session/session.module';
 import { AppointmentBlankResolver } from './resolver/appointmentBlank.resolver';
+import { AppointmenResultsResolver } from './resolver/parts/appointmentResults.resolver';
 import { AppointmentBlankService } from './service/appointmentBlank.service';
+import { AppointmenResultsService } from './service/parts/appointmentResult.service';
 
 @Module({
     imports: [MongoModule, UploadFileModule, SessionModule, PreAuthModule],
-    providers: [AppointmentBlankService, AppointmentBlankResolver],
+    providers: [
+        AppointmentBlankService,
+        AppointmentBlankResolver,
+        AppointmenResultsResolver,
+        AppointmenResultsService,
+    ],
     exports: [AppointmentBlankService],
 })
 export class AppointmentBlankModule {}
