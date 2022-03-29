87e9063ead45f58fc7826a7adb0d3e079571aee9
irlen.turlykhanov <irlen.turlykhanov@gmail.com>
2022-03-29 16:32:28 +0600

    fix: mongo in notifications

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CalendarModule } from '../helpers/calendar/calendar.module';
+import { MongoModule } from '../helpers/database/mongo.module';
 import { MailGunModule } from '../helpers/mailgun/mailgun.module';
 import { NotificationService } from './service/notification.service';
 
 @Module({
-    imports: [MailGunModule, CalendarModule],
+    imports: [MailGunModule, CalendarModule, MongoModule],
     providers: [NotificationService],
     exports: [NotificationService],
 })
