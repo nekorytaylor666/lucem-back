555b7c27b0b96c6b1547613bfa39ad1774e123cf
irlen.turlykhanov <irlen.turlykhanov@gmail.com>
2022-03-29 21:02:58 +0600

    fix

--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PreAuthModule } from '../helpers/auth/auth.module';
 import { MongoModule } from '../helpers/database/mongo.module';
+import { NotificationModule } from '../notification/notification.module';
 import { CommentResolver } from './resolver/comment.resolver';
 import { CommentService } from './service/comment.service';
 
 @Module({
-    imports: [MongoModule, PreAuthModule],
+    imports: [MongoModule, PreAuthModule, NotificationModule],
     providers: [CommentService, CommentResolver],
     exports: [CommentService],
 })
